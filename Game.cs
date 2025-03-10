1f921c6241fefbd17d6729804b4236a2e81ebb4b
TomHenderson21 <tomcallumhenderson@gmail.com>
2025-03-10 22:24:41 +0000

    Added comments, added room option, if else statement

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Media;
 
 namespace DungeonExplorer
@@ -12,53 +13,59 @@ namespace DungeonExplorer
         {
             Console.WriteLine("Welcome to the Dungeon Explorer Game!");
             Console.Write("Enter your preferred character name: ");
-            string name = Console.ReadLine();
-            player = new Player(name, 100);
+            string name = Console.ReadLine();   //Get the users input for the character name they want
+            player = new Player(name, 100); //Give the player a name and health
             currentRoom = new Room("small dark room with a flickering flame in the middle that ever so slightly lightens up the room, and the sound of water pattering against the floor echos throughout.");
-            // Initialize the game with one room and one player
         }
-        public void Start()
+        public void Start() 
         {
-            // Change the playing logic into true and populate the while loop
             bool playing = true;
             Console.WriteLine($"Welcome, Tarnished {player.Name}. You are about to embark on your new journey in these dungeons but be careful on your discovery as who knows what you may find, remember you only have {player.Health} health.");
-            Console.WriteLine($"You are currently in a {currentRoom.GetDescription()}.");
+            Console.WriteLine($"You are currently in a {currentRoom.GetDescription()}.");   //Give the user a description of the room 
             Console.WriteLine("To begin your journey you will have to decide what you want to do.");
             while (playing)
             {
-                Console.WriteLine("Please choose what you want to do from the following...");
+                Console.WriteLine("\nPlease choose what you want to do from the following..."); //Tell the user that they will now have to choose from the following options, shown on a new line each time for better readability
                 Console.WriteLine("1) Room description");
                 Console.WriteLine("2) Explore room ");
                 Console.WriteLine("3) Check inventory");
                 Console.WriteLine("4) Check health");
-                Console.WriteLine("5) Exit the game");
+                Console.WriteLine("5) Enter the next room");
+                Console.WriteLine("6) Exit the game");
                 Console.Write("Please enter your choice with the corresponding number: ");
                 
-                string userChoice = Console.ReadLine();
-
-                switch (userChoice)
+                int userChoice = Convert.ToInt32(Console.ReadLine()); //Get the userChoice input as an integer
+                
+                if (userChoice == 1)
                 {
-                    case "1":
-                        Console.WriteLine($"You are in a {currentRoom.GetDescription()}.");
-                        break;
-                    case "2":
-                        Console.Write("During your search around the room");
-                        player.PickUpItem();
-                        break;
-                    case "3":
-                        Console.Write("You chose to check your inventory, ");
-                        player.InventoryContents();
-                        break;
-                    case "4":
-                        Console.WriteLine($"You are currently at {player.Health} health.");
-                        break;
-                    case "5":
-                        Console.WriteLine("You are about to exit the game. Thanks for playing.");
-                        playing = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice, please choose a valid option");
-                        continue;
+                    Console.WriteLine($"Your are in {currentRoom.GetDescription()}.");  //Room description shown again 
+                }
+                else if (userChoice == 2)
+                {
+                    Console.WriteLine("During your search around the room");    //Tells user what they found during room search
+                    player.PickUpItem();
+                }
+                else if (userChoice == 3)
+                {
+                    Console.WriteLine("You chose to check your inventory, ");   //Shows the user what is in their inventory
+                    player.InventoryContents();
+                }
+                else if (userChoice == 4)
+                {
+                    Console.WriteLine($"You are currently at {player.Health} health out of 100.");  //Shows the user their health
+                }
+                else if (userChoice == 5)
+                {
+                    Console.WriteLine("You chose to enter the next room, this is currently unavailable.");  //Tell the user that this action is unavailable
+                }
+                else if (userChoice == 6)
+                {
+                    Console.WriteLine("You chose to exit the game.");   //Lets user exit game
+                    playing = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Please try again '{userChoice}' is an invalid choice, please enter a valid choice. ");  //If none of the userChoices ran tell the user to enter a valid choice
                 }
             }
         }
